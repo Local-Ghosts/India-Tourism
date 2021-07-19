607a4f852d4d8cacf6f1ef3992be90aef7ed55b9
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-07-19 11:35:45 +0530

    Added Header Component

--- tourism/src/App.js
+++ tourism/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Display from "./components/Display";
 function App() {
   return (
     <div className="App">
-      <h1>Landing Page</h1>
+      <Display />
     </div>
   );
 }
---
+++ tourism/src/components/Display.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Display.css'
+
+class Display extends React.Component{
+    constructor() {
+        super();
+        
+        this.state = {
+            firstActive:false,    
+            secondActive:false,
+            firsthide:false,
+            
+        };
+        
+    }
+    
+    componentDidMount()
+    {    
+        
+        this.setState({firstActive:true});
+
+        setTimeout(()=>{
+            this.setState({firstActive:false})
+            this.setState({firsthide:true})
+            this.setState({secondActive:true})
+           }, 3000);
+    }
+
+    render(){
+        return (
+        <div>
+            <section className={`first${this.state.firstActive?" active":""}${this.state.firsthide ?" hide":""}`}>
+
+            </section>
+            <section className={`second ${this.state.secondActive?"active":""}`}>
+            </section>
+            <div class="sp-container">
+	        <div class="sp-content">
+		    <div class="sp-globe"></div>
+		    <h2 class="frame-1">Welcome</h2>
+		    <h2 class="frame-2">To Our India</h2>
+		    <h2 class="frame-3">The Place To be</h2>
+		    <h2 class="frame-4">Come</h2>
+		    <h2 class="frame-5">
+			<span>Live,</span>
+			<span>Travel,</span>
+			<span>Love</span>
+		    </h2>
+		
+	</div>
+</div>
+
+        </div>
+    )
+    }
+}
+
+export default Display
